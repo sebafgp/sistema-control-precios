b9f39e612ce347f2cfd3d048bec3f03dc89f2635
Macher <claudio.macher1801@alumnos.ubiobio.cl>
2021-10-21 18:26:03 -0300

    inicio

---
+++ src/test/java/com/backendigans/demo/SistemaControlDePreciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendigans.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaControlDePreciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
