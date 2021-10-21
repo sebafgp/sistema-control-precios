19b13202dfd8d913da1fb57f21e17d92b90b0d56
sebafgp <41242748+sebafgp@users.noreply.github.com>
2021-10-21 20:32:43 -0300

    add colaborador

--- src/main/java/com/backendigans/demo/model/Colaborador.java
+++ src/main/java/com/backendigans/demo/model/Colaborador.java
@@ -1,5 +1,44 @@
 package com.backendigans.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "colaborador")
 public class Colaborador {
+    private String email, contrasena, nickname;
+    private int puntos, reputacion;
+
+    public Colaborador(){
+    }
+
+    public Colaborador (String email, String contrasena, String nickname, int puntos, int reputacion){
+        this.email = email;
+        this.contrasena = contrasena;
+        this.nickname = nickname;
+        this.puntos = puntos;
+        this.reputacion = reputacion;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    public String getEmail() {
+        return email;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public int getPuntos() {
+        return puntos;
+    }
+    public int getReputacion() {
+        return reputacion;
+    }
     
 }
