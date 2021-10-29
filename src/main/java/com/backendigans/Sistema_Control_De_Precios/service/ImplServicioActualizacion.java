9fed146b61632cbbd29caddbc9548f942faf84e5
Macher <claudio.macher1801@alumnos.ubiobio.cl>
2021-10-29 19:47:42 -0300

    HU5-Servicio Repositorio Actualización

---
+++ src/main/java/com/backendigans/Sistema_Control_De_Precios/service/ImplServicioActualizacion.java
@@ -0,0 +1,23 @@
+package com.backendigans.Sistema_Control_De_Precios.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.backendigans.Sistema_Control_De_Precios.model.Actualizacion;
+import com.backendigans.Sistema_Control_De_Precios.repository.RepositorioActualizacion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ImplServicioActualizacion implements ServicioActualizacion{
+    @Autowired
+    private RepositorioActualizacion actualizacionRepository;
+
+    @Override
+    public List<Actualizacion> listAllActualizacion() {
+        return actualizacionRepository.findAll();
+    }
+}
---
+++ src/main/java/com/backendigans/Sistema_Control_De_Precios/service/ServicioActualizacion.java
@@ -0,0 +1,10 @@
+package com.backendigans.Sistema_Control_De_Precios.service;
+
+import java.util.List;
+
+import com.backendigans.Sistema_Control_De_Precios.model.Actualizacion;
+
+public interface ServicioActualizacion {
+
+    public List<Actualizacion> listAllActualizacion();
+}
