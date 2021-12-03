aa35fd09c6ef9da30b89de556b8f055fd742f86e
Macher <claudio.macher1801@alumnos.ubiobio.cl>
2021-12-03 17:52:48 -0300

    Test unitarios HU07

--- src/test/java/com/backendigans/Sistema_Control_De_Precios/service/ServicioColaboradorTest.java
+++ src/test/java/com/backendigans/Sistema_Control_De_Precios/service/ServicioColaboradorTest.java
@@ -10,6 +10,7 @@ import com.backendigans.Sistema_Control_De_Precios.model.Actualizacion;
 import com.backendigans.Sistema_Control_De_Precios.model.Colaborador;
 import com.backendigans.Sistema_Control_De_Precios.model.Producto;
 import com.backendigans.Sistema_Control_De_Precios.repository.RepositorioColaborador;
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -162,26 +163,44 @@ public class ServicioColaboradorTest {
     }
 
 
-
-   /* @Test
-    @DisplayName("Buscar reputación y actualizaciones por nickname - Lista Existe")
-    void siInvocoGetColaboradorRepActYExisteEntoncesRetornarListaNoVacia(){
+    /*  HU_07   */
+    @Test
+    @DisplayName("Buscar colaborador por nickname - Colaborador existe")
+    void siInvocoGetColaboradorByNicknameYExisteEntoncesRetornarColaboradorConEseNickname(){
 
         // Arrange
-        List<Object> resultado;
-        List<Object> lista = cargarDatosLista();
+        Colaborador resultado;
+        Colaborador colaborador = crearColaborador();
 
-        String nickname = "Marco";
+        String nickname = "nick";
 
-        when(colaboradorService.getColaboradorRepAct(nickname).thenReturn(lista));
+        when(colaboradorRepository.findFirstByNickname(nickname)).thenReturn(colaborador);
 
         //Act
-        resultado = colaboradorService.getColaboradorRepAct(nickname);
+        
+        resultado = colaboradorService.getColaboradorByNickname(nickname);
 
         //Assert
         assertNotNull(resultado);
-        assertEquals(lista.size(), resultado.size());
-    }*/
+        assertEquals(colaborador.getEmail(), resultado.getEmail());
+    }
+
+    @Test
+    @DisplayName("Buscar colaborador por nickname - Colaborador no existe")
+    void siInvocoGetColaboradorByNicknameYEsteNoExisteEntoncesRetornarNull(){
+
+        // Arrange
+        Colaborador resultado;
+        String nickname = "nick";
+
+        when(colaboradorRepository.findFirstByNickname(nickname)).thenReturn(null);
+
+        //Act
+        resultado = colaboradorService.getColaboradorByNickname(nickname);
+
+        //Assert
+        assertNull(resultado);
+    }
 
     /* Funciones Utilidad */
 
@@ -189,26 +208,4 @@ public class ServicioColaboradorTest {
         Colaborador c = new Colaborador("ex@mail.com", "password", "nick");
         return c;
     }
-
-    /*private List<Object> cargarDatosLista() {
-        Colaborador colaborador = new Colaborador(1, "marco@mail.com", "123", "Marco", 0, 0);
-        Producto producto = new Producto(1, "Tallarines", "Luchetti", 1, 1000, LocalDateTime.parse("2021-11-24T12:00:00"));
-        Actualizacion actualizacion = new Actualizacion(1, colaborador, producto, 2000, LocalDateTime.parse("2021-11-24T12:00:00"), 0);
-        List<Object> ob = new ArrayList<>(); 
-
-        Set<Producto> productos = new HashSet<>();
-        Set<Actualizacion> actualizaciones = new HashSet<>();
-
-        productos.add(producto);
-        actualizaciones.add(actualizacion);
-
-        colaborador.setProductos(productos);
-        colaborador.setActualizacion(actualizaciones);
-
-        ob.add(colaborador.getNickname());
-        ob.add(colaborador.getReputacion());
-        ob.add(colaborador.getActualizaciones());
-
-        return ob;
-    } */
 }
